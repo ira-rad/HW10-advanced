05718e45a25e544ba97d0d963caf627783046335
Ira <irarad1994@gmail.com>
2020-08-20 13:54:12 +0300

    edited errors

--- js/script.js
+++ js/script.js
@@ -5,38 +5,27 @@ function getAudio(source){
 }
 
 
-document.getElementById("buttonQ").addEventListener('click', () => {
-  getAudio('audio/11.mp3')
-});
-document.getElementById("buttonW").addEventListener('click', () => {
-  getAudio('audio/22.mp3')
-});
-document.getElementById("buttonE").addEventListener('click', () => {
-  getAudio('audio/33.mp3')
-});
-document.getElementById("buttonR").addEventListener('click', () => {
-  getAudio('audio/44.mp3')
-});
-document.getElementById("buttonT").addEventListener('click', () => {
-  getAudio('audio/55.mp3')
-});
-document.getElementById("buttonY").addEventListener('click', () => {
-  getAudio('audio/66.mp3')
-});
+const myButton = document.querySelectorAll('button');
+myButton.forEach((elem, index) =>{
+  elem.addEventListener('click', () =>{
+    getAudio(`audio/1${index + 1}.mp3`);
+  })
+})
+
 
 document.addEventListener("keydown", function (e) {
   return e.keyCode == 81 
     ? getAudio('audio/11.mp3')
     : e.keyCode == 87
-    ? getAudio('audio/22.mp3')
+    ? getAudio('audio/12.mp3')
     : e.keyCode == 69
-    ? getAudio('audio/33.mp3')
+    ? getAudio('audio/13.mp3')
     : e.keyCode == 82
-    ? getAudio('audio/44.mp3')
+    ? getAudio('audio/14.mp3')
     : e.keyCode == 84
-    ? getAudio('audio/55.mp3')
+    ? getAudio('audio/15.mp3')
     : e.keyCode == 89
-    ? getAudio('audio/66.mp3')
+    ? getAudio('audio/16.mp3')
     : document.getElementById('')
 });
  
